ffb0bdd518018d15af54b3f54e1c07b2b1f50d53
Alexis Castillo <alexis.castillo.pimienta@gmail.com>
2022-09-05 19:19:02 -0600

    quito los permisos de jwt en products

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 
 const { getProducts, postProduct, putProduct, deleteProduct } = require("../controllers/products.ctrl");
 
-const { validarJWT } = require('../middleware/jwt.middleware');
+//const { validarJWT } = require('../middleware/jwt.middleware');
 
 const router = Router();
 
-router.get("/", validarJWT, getProducts);
+// router.get("/", validarJWT, getProducts);
+router.get("/", getProducts);
 
 router.post("/", postProduct);
 
